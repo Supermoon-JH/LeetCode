c8f1ba73e2b99cf77b5c449d5b613a9ec479d7b2
MOON JIHWAN <81791860+Supermoon-JH@users.noreply.github.com>
2024-09-25 01:59:06 +0900

    Time: 34 ms (88.06%), Space: 16.5 MB (53.37%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,5 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:] = nums2
-        nums1.sort()
+        # nums1[m:] = nums2
+        # nums1.sort()
+
+        m_idx = m - 1
+        n_idx = n - 1
+        right = m + n - 1
+
+        while n_idx >= 0:
+            if m_idx >= 0 and nums1[m_idx] > nums2[n_idx]:
+                nums1[right] = nums1[m_idx]
+                m_idx -= 1
+            else:
+                nums1[right] = nums2[n_idx]
+                n_idx -= 1
+            
+            right -= 1
