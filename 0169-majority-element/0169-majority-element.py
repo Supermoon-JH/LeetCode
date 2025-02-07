f84140d2cee8bcce9cd68f36ad89b20ba90f597b
문지환 <81791860+Supermoon-JH@users.noreply.github.com>
2025-02-07 17:53:50 +0900

    Time: 2 ms (84.83%), Space: 19.7 MB (17.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,11 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
-        freq = defaultdict(int)
+        # n = len(nums)
+        # freq = defaultdict(int)
 
-        for i in nums:
-            freq[i] += 1
+        # for i in nums:
+        #     freq[i] += 1
 
-        for key, value in freq.items():
-            if value > math.floor(n / 2):
-                return key
+        # for key, value in freq.items():
+        #     if value > math.floor(n / 2):
+        #         return key
+
+        nums.sort()
+        return nums[math.floor(len(nums) / 2)]
