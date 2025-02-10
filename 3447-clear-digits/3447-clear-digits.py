2691059b0b494280756736878eff62066035c95e
문지환 <81791860+Supermoon-JH@users.noreply.github.com>
2025-02-10 12:24:08 +0900

    Time: 3 ms (30.49%), Space: 17.7 MB (70.7%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -15,12 +15,28 @@ class Solution:
 
         # return "".join(s_list)
         # brute force
-        s_list = []
 
-        for i in s:
-            if i.isdigit() and s_list:
-                s_list.pop()
+        # s_list = []
+
+        # for i in s:
+        #     if i.isdigit() and s_list:
+        #         s_list.pop()
+        #     else:
+        #         s_list.append(i)
+
+        # return "".join(s_list)
+        # stack
+
+        answer_len = 0
+        s = list(s)
+
+        for i in range(len(s)):
+            if s[i].isdigit():
+                answer_len = max(answer_len - 1, 0)
             else:
-                s_list.append(i)
+                s[answer_len] = s[i]
+                answer_len += 1
 
-        return "".join(s_list)
+        s = s[:answer_len]
+
+        return "".join(s)
