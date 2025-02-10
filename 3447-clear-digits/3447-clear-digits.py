ec27df169d1f288a7565fb572c1a3f399c4445c2
문지환 <81791860+Supermoon-JH@users.noreply.github.com>
2025-02-10 12:08:41 +0900

    Time: 0 ms (100%), Space: 17.7 MB (43.55%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -1,16 +1,26 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        s_list = list(s)
-        index = 0
+        # s_list = list(s)
+        # index = 0
 
-        while index < len(s_list):
-            if s_list[index].isdigit():
-                del s_list[index]
-                if index > 0:
-                    del s_list[index - 1]
-                    index -= 1
+        # while index < len(s_list):
+        #     if s_list[index].isdigit():
+        #         del s_list[index]
+        #         if index > 0:
+        #             del s_list[index - 1]
+        #             index -= 1
+        #     else:
+        #         index += 1
+
+
+        # return "".join(s_list)
+        # brute force
+        s_list = []
+
+        for i in s:
+            if i.isdigit() and s_list:
+                s_list.pop()
             else:
-                index += 1
-
+                s_list.append(i)
 
         return "".join(s_list)
