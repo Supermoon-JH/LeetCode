46e589a58bb9b4760c0e1fcd4f8739f356297291
문지환 <81791860+Supermoon-JH@users.noreply.github.com>
2025-02-11 10:57:07 +0900

    Time: 3 ms (56.31%), Space: 18 MB (15.89%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        def delete_part(s):
+            new = s.replace(part, "", 1)
+            return new
+
+        answer = s
+        while True:
+            if answer == delete_part(answer):
+                break
+            answer = delete_part(answer)
+            print(answer)
+            
+        return answer
