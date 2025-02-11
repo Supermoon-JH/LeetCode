dbfb98faf00dc77a4399fa181edd0c5949c841fe
문지환 <81791860+Supermoon-JH@users.noreply.github.com>
2025-02-11 11:00:49 +0900

    Time: 0 ms (100%), Space: 18 MB (32.79%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.py
@@ -1,14 +1,19 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        def delete_part(s):
-            new = s.replace(part, "", 1)
-            return new
+        # def delete_part(s):
+        #     new = s.replace(part, "", 1)
+        #     return new
 
-        answer = s
-        while True:
-            if answer == delete_part(answer):
-                break
-            answer = delete_part(answer)
-            print(answer)
+        # answer = s
+        # while True:
+        #     if answer == delete_part(answer):
+        #         break
+        #     answer = delete_part(answer)
+        #     print(answer)
             
-        return answer
+        # return answer
+        # my answer
+
+        while part in s:
+            s = s.replace(part, "", 1)
+        return s
